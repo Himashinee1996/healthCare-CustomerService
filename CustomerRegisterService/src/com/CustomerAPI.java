dda10e07feda095d45caef6d3d1d1d74d412695c
Himashinee1996 <himashineethareendra0502@gmail.com>
2020-05-05 08:42:02 +0530

    customer api

--- CustomerRegisterService/src/com/CustomerAPI.java
+++ CustomerRegisterService/src/com/CustomerAPI.java
@@ -1,5 +1,6 @@
 package com;
 
+import com.Customer;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,49 +8,100 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * Servlet implementation class CustomerAPI
  */
 @WebServlet("/CustomerAPI")
 public class CustomerAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CustomerAPI() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	Customer cusObj = new Customer();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public CustomerAPI() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		String output = cusObj.insertCustomer(request.getParameter("firstName"), request.getParameter("lastName"),
+				request.getParameter("dob"), request.getParameter("age"), request.getParameter("gender"),
+				request.getParameter("address"), request.getParameter("phone"), request.getParameter("email"),
+				request.getParameter("username"), request.getParameter("password"));
+
+		response.getWriter().write(output);
 	}
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		Map paras = getParasMap(request);
+
+		String output = cusObj.updateCustomer(paras.get("hidCustomerIDSave").toString(),
+				paras.get("firstName").toString(), paras.get("lastName").toString(), paras.get("dob").toString(),
+				paras.get("age").toString(), paras.get("gender").toString(), paras.get("address").toString(),
+				paras.get("phone").toString(), paras.get("email").toString(), paras.get("username").toString(),
+				paras.get("password").toString());
+
+		response.getWriter().write(output);
 	}
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		Map paras = getParasMap(request);
+		String output = cusObj.deleteCustomer(paras.get("userID").toString());
+		response.getWriter().write(output);
+	}
+
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) {
+		Map<String, String> map = new HashMap<String, String>();
+		try {
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			String[] params = queryString.split("&");
+			for (String param : params) {
+
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+		} catch (Exception e) {
+		}
+		return map;
 	}
 
 }
